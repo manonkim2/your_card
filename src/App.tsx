1d36edaee6187dbf6499d48bb51b2b53ea22f49f
Manon Kim <kim.manon2@gmail.com>
2024-03-31 02:39:42 +0900

    [style] navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@pages/Home'
 import TestPage from '@pages/Test'
 import CardPage from './pages/Card'
+import SigninPage from './pages/Signin'
+import SignupPage from './pages/Signup'
+import Navbar from './components/shared/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/card/:id" Component={CardPage} />
         <Route path="/test" Component={TestPage} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
       </Routes>
     </BrowserRouter>
   )
